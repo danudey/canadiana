83401eb079ab56c5db0784c0acb754505cc0c5b1
Daniel Fox <dan.fox@tigera.io>
2024-07-26 12:35:25 -0700

    Add non-beta apple music domain

--- src/apple-music.js
+++ src/apple-music.js
@@ -1,3 +1,3 @@
-if (document.location.host == 'beta.music.apple.com' && document.location.pathname.startsWith("/us/")) {
+if (document.location.host.endsWith('music.apple.com') && document.location.pathname.startsWith("/us/")) {
     document.location.replace(document.location.toString().replace("/us/","/ca/"))
 }
